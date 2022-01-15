1a9ef5a13fcef365089a138de50c9e38162e6362
mdcrodrigo <mdcrodrigo@hotmail.com>
2022-01-15 11:56:04 -0300

    Atualizando

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -8,6 +8,7 @@ Mostre uma mensagem caso o valor seja inválido
 e continue pedindo
 até que o usuário informe um valor válido.
 */
+
 public class Ex2_Nota {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
